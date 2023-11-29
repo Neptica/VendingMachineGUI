00ce2da31dd2536955c8e008c3770387452a3a49
xavierpalmer21 <xpalmer@blc.edu>
2023-11-29 12:29:47 -0600

    first

---
+++ VendingMachineGUI/VendingMachineGUI/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace VendingMachineGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.milkyway = new System.Windows.Forms.PictureBox();
+            this.funyons = new System.Windows.Forms.PictureBox();
+            this.cheetos = new System.Windows.Forms.PictureBox();
+            this.ranchDoritos = new System.Windows.Forms.PictureBox();
+            this.nachoDoritos = new System.Windows.Forms.PictureBox();
+            this.snickers = new System.Windows.Forms.PictureBox();
+            this.vendingMachineBottom = new System.Windows.Forms.PictureBox();
+            this.vendingMachine = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.milkyway)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.funyons)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cheetos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ranchDoritos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nachoDoritos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snickers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vendingMachineBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vendingMachine)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // milkyway
+            // 
+            this.milkyway.Image = global::VendingMachineGUI.Properties.Resources.milkway;
+            this.milkyway.Location = new System.Drawing.Point(140, 120);
+            this.milkyway.Name = "milkyway";
+            this.milkyway.Size = new System.Drawing.Size(23, 84);
+            this.milkyway.TabIndex = 7;
+            this.milkyway.TabStop = false;
+            // 
+            // funyons
+            // 
+            this.funyons.Image = global::VendingMachineGUI.Properties.Resources.funyons;
+            this.funyons.Location = new System.Drawing.Point(262, 35);
+            this.funyons.Name = "funyons";
+            this.funyons.Size = new System.Drawing.Size(42, 67);
+            this.funyons.TabIndex = 6;
+            this.funyons.TabStop = false;
+            // 
+            // cheetos
+            // 
+            this.cheetos.Image = global::VendingMachineGUI.Properties.Resources.cheetos;
+            this.cheetos.Location = new System.Drawing.Point(200, 37);
+            this.cheetos.Name = "cheetos";
+            this.cheetos.Size = new System.Drawing.Size(39, 66);
+            this.cheetos.TabIndex = 5;
+            this.cheetos.TabStop = false;
+            // 
+            // ranchDoritos
+            // 
+            this.ranchDoritos.Image = global::VendingMachineGUI.Properties.Resources.download;
+            this.ranchDoritos.Location = new System.Drawing.Point(62, 52);
+            this.ranchDoritos.Name = "ranchDoritos";
+            this.ranchDoritos.Size = new System.Drawing.Size(33, 50);
+            this.ranchDoritos.TabIndex = 4;
+            this.ranchDoritos.TabStop = false;
+            // 
+            // nachoDoritos
+            // 
+            this.nachoDoritos.Image = global::VendingMachineGUI.Properties.Resources.nachoDoritos;
+            this.nachoDoritos.Location = new System.Drawing.Point(132, 52);
+            this.nachoDoritos.Name = "nachoDoritos";
+            this.nachoDoritos.Size = new System.Drawing.Size(41, 50);
+            this.nachoDoritos.TabIndex = 3;
+            this.nachoDoritos.TabStop = false;
+            // 
+            // snickers
+            // 
+            this.snickers.Image = global::VendingMachineGUI.Properties.Resources.pngimg3;
+            this.snickers.Location = new System.Drawing.Point(73, 124);
+            this.snickers.Name = "snickers";
+            this.snickers.Size = new System.Drawing.Size(22, 80);
+            this.snickers.TabIndex = 2;
+            this.snickers.TabStop = false;
+            // 
+            // vendingMachineBottom
+            // 
+            this.vendingMachineBottom.Image = global::VendingMachineGUI.Properties.Resources.Capture;
+            this.vendingMachineBottom.Location = new System.Drawing.Point(-48, 438);
+            this.vendingMachineBottom.Name = "vendingMachineBottom";
+            this.vendingMachineBottom.Size = new System.Drawing.Size(479, 160);
+            this.vendingMachineBottom.TabIndex = 1;
+            this.vendingMachineBottom.TabStop = false;
+            // 
+            // vendingMachine
+            // 
+            this.vendingMachine.BackgroundImage = global::VendingMachineGUI.Properties.Resources.pngtree_vending_machine_blue_automatic_machine_png_image_3979327;
+            this.vendingMachine.Location = new System.Drawing.Point(-51, -14);
+            this.vendingMachine.Name = "vendingMachine";
+            this.vendingMachine.Size = new System.Drawing.Size(482, 629);
+            this.vendingMachine.TabIndex = 0;
+            this.vendingMachine.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 618);
+            this.Controls.Add(this.milkyway);
+            this.Controls.Add(this.funyons);
+            this.Controls.Add(this.cheetos);
+            this.Controls.Add(this.ranchDoritos);
+            this.Controls.Add(this.nachoDoritos);
+            this.Controls.Add(this.snickers);
+            this.Controls.Add(this.vendingMachineBottom);
+            this.Controls.Add(this.vendingMachine);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.milkyway)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.funyons)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cheetos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ranchDoritos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nachoDoritos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snickers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vendingMachineBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vendingMachine)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox vendingMachine;
+        private System.Windows.Forms.PictureBox vendingMachineBottom;
+        private System.Windows.Forms.PictureBox snickers;
+        private System.Windows.Forms.PictureBox nachoDoritos;
+        private System.Windows.Forms.PictureBox ranchDoritos;
+        private System.Windows.Forms.PictureBox cheetos;
+        private System.Windows.Forms.PictureBox funyons;
+        private System.Windows.Forms.PictureBox milkyway;
+    }
+}
+
