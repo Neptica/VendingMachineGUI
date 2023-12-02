c9e72e1ded69947e8d451b9e8b2bd74ffb647199
Michael Mumme <neptica@hotmail.com>
2023-12-01 20:49:51 -0600

    Fixed crashing on purchase when less than 1 of each letter and number have been entered

--- VendingMachineGUI/VendingMachineGUI/Form1.cs
+++ VendingMachineGUI/VendingMachineGUI/Form1.cs
@@ -10,220 +10,224 @@ using System.Windows.Forms;
 
 namespace VendingMachineGUI
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    private const bool DEVELOPERMODE = true;
+    private string[] userinput = { " ", " " };
+    private string pwAttempt = "";
+    private string password = "A1D4B2C3";
+    private bool privileged = false;
+    private bool firstTime = true;
+
+    public Form1()
     {
-        private const bool DEVELOPERMODE = true;
-        private string[] userinput = {" ", " "};
-        private string pwAttempt = ""; 
-        private string password = "A1D4B2C3";
-        private bool privileged = false;
-        private bool firstTime = true;
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private VendingMachine machine;
-
-        private void adjustDisplay(int input)
-        {
-            pwAttempt += input;
-            userinput[1] = Convert.ToString(input);
-            DisplayUserInput();
-        }
-        private void adjustDisplay(string input)
-        {
-            pwAttempt += input;
-            userinput[0] = input;
-            DisplayUserInput();
-        }
-        private void adjustDisplay()
-        {
-            userinput[0] = " ";
-            userinput[1] = " ";
-            pwAttempt = "";
-            DisplayUserInput();
-        }
-        private void DisplayUserInput()
-        {
-            if (DEVELOPERMODE) textBox2.Text = pwAttempt; // this is displayed for developer convenience
-            textBox1.Text = string.Join("", userinput);
-        }
-        private void button3_Click(object sender, EventArgs e)
-        {
-            adjustDisplay("A");
-        }
-        private void button1_Click(object sender, EventArgs e)
-        {
-            adjustDisplay("B");
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            adjustDisplay("C");
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            adjustDisplay("D");
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            adjustDisplay(1);
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            adjustDisplay(2);
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            adjustDisplay(3);
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            adjustDisplay(4);
-        }
-
-        private void button9_Click(object sender, EventArgs e) // Hashtag (Clear and Login Attempt)
-        {
-            if (pwAttempt == password)
-            {
-                privileged = !privileged; // this flips the mode (exits admin mode the using the same password)
-                if (privileged)
-                {
-                    textBox2.Text = "Priviliged Mode";
-                    button10.Text = "Restock";
-                    button11.Text = "Logout";
-                }
-                else
-                {
-                    textBox2.Text = "Customer Mode";
-                    button10.Text = "Eject";
-                    button11.Text = "Purchase";
-                }
-                    pwAttempt = "";
-                userinput[0] = " ";
-                userinput[1] = " ";
-            }
-            else
-            {
-                adjustDisplay();
-            }
-        }
-
-        private void button10_Click(object sender, EventArgs e) // Eject
-        {
-            if (privileged)
-            {
-                if (firstTime)
-                {
-                    PictureBox[] pictureArray = { ranchDoritosFalling, ranchDoritos }; // Add all the pictures in the right order.
-                    machine = new VendingMachine(pictureArray);
-                }
-                machine.restockProducts();
-                textBox2.Text = "Restocked"; // functionality not added yet
-            }
-            else
-            {
-                adjustDisplay();
-            }
-        }
-
-        private void button11_Click(object sender, EventArgs e) // Purchase
-        {
-            if (privileged)
-            {
-                textBox2.Text = "Customer Mode";
-                button10.Text = "Eject";
-                button11.Text = "Purchase";
-                privileged = !privileged;
-                textBox2.Text = "Logged out"; // functionality not added yet
-            }
-            else
-            {
-                string input = textBox1.Text;
-                if (input.Length == 2) 
-                {
-                    int index = 0;
-                    switch (input)
-                    {
-                        case "A1":
-                            // code to purchase the first element
-                            index = 0;
-                            break;
-                        case "A2":
-                            // code to purchase the second element, etc
-                            index = 1;
-                            break;
-                        case "A3":
-                            // code to purchase the first element
-                            index = 2;
-                            break;
-                        case "A4":
-                            // code to purchase the second element, etc
-                            index = 3;
-                            break;
-                        case "B1":
-                            // code to purchase the first element
-                            index = 4;
-                            break;
-                        case "B2":
-                            // code to purchase the second element, etc
-                            index = 5;
-                            break;
-                        case "B3":
-                            // code to purchase the first element
-                            index = 6;
-                            break;
-                        case "B4":
-                            // code to purchase the second element, etc
-                            index = 7;
-                            break;
-                        case "C1":
-                            // code to purchase the first element
-                            index = 8;
-                            break;
-                        case "C2":
-                            // code to purchase the second element, etc
-                            index = 9;
-                            break;
-                        case "C3":
-                            // code to purchase the first element
-                            index = 10;
-                            break;
-                        case "C4":
-                            // code to purchase the second element, etc
-                            index = 11;
-                            break;
-                        case "D1":
-                            // code to purchase the first element
-                            index = 12;
-                            break;
-                        case "D2":
-                            // code to purchase the second element, etc
-                            index = 13;
-                            break;
-                        case "D3":
-                            // code to purchase the first element
-                            index = 14;
-                            break;
-                        case "D4":
-                            // code to purchase the second element, etc
-                            index = 15;
-                            break;
-                    }
-                    textBox2.Text = machine.buyProduct(index);
-                }
-                else
-                {
-                    textBox2.Text = string.Format("Not a valid item {0}", textBox1.Text.Length);
-                }
-            }
-        }
+      InitializeComponent();
     }
+
+    private VendingMachine machine;
+
+    private void adjustDisplay(int input)
+    {
+      pwAttempt += input;
+      userinput[1] = Convert.ToString(input);
+      DisplayUserInput();
+    }
+    private void adjustDisplay(string input)
+    {
+      pwAttempt += input;
+      userinput[0] = input;
+      DisplayUserInput();
+    }
+    private void adjustDisplay()
+    {
+      userinput[0] = "";
+      userinput[1] = "";
+      pwAttempt = "";
+      DisplayUserInput();
+    }
+    private void DisplayUserInput()
+    {
+      if (DEVELOPERMODE) textBox2.Text = pwAttempt; // this is displayed for developer convenience
+      textBox1.Text = string.Join("", userinput);
+    }
+    private void button3_Click(object sender, EventArgs e)
+    {
+      adjustDisplay("A");
+    }
+    private void button1_Click(object sender, EventArgs e)
+    {
+      adjustDisplay("B");
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      adjustDisplay("C");
+    }
+
+    private void button7_Click(object sender, EventArgs e)
+    {
+      adjustDisplay("D");
+    }
+
+    private void button5_Click(object sender, EventArgs e)
+    {
+      adjustDisplay(1);
+    }
+
+    private void button6_Click(object sender, EventArgs e)
+    {
+      adjustDisplay(2);
+    }
+
+    private void button4_Click(object sender, EventArgs e)
+    {
+      adjustDisplay(3);
+    }
+
+    private void button8_Click(object sender, EventArgs e)
+    {
+      adjustDisplay(4);
+    }
+
+    private void button9_Click(object sender, EventArgs e) // Hashtag (Clear and Login Attempt)
+    {
+      if (pwAttempt == password)
+      {
+        privileged = !privileged; // this flips the mode (exits admin mode the using the same password)
+        if (privileged)
+        {
+          textBox2.Text = "Priviliged Mode";
+          button10.Text = "Restock";
+          button11.Text = "Logout";
+        }
+        else
+        {
+          textBox2.Text = "Customer Mode";
+          button10.Text = "Eject";
+          button11.Text = "Purchase";
+        }
+        pwAttempt = "";
+        userinput[0] = " ";
+        userinput[1] = " ";
+      }
+      else
+      {
+        adjustDisplay();
+      }
+    }
+
+    private void button10_Click(object sender, EventArgs e) // Eject
+    {
+      if (privileged)
+      {
+        if (firstTime)
+        {
+          PictureBox[] pictureArray = { ranchDoritosFalling, ranchDoritos }; // Add all the pictures in the right order.
+          machine = new VendingMachine(pictureArray);
+        }
+        machine.restockProducts();
+        textBox2.Text = "Restocked"; // functionality not added yet
+      }
+      else
+      {
+        adjustDisplay();
+      }
+    }
+
+    private void button11_Click(object sender, EventArgs e) // Purchase
+    {
+      if (privileged)
+      {
+        textBox2.Text = "Customer Mode";
+        button10.Text = "Eject";
+        button11.Text = "Purchase";
+        privileged = !privileged;
+        textBox2.Text = "Logged out"; // functionality not added yet
+      }
+      else
+      {
+        string input = textBox1.Text.Replace(" ", "");
+        textBox2.Text = Convert.ToString(input.Length);
+        if (input.Length == 2)
+        {
+          int index = 99;
+          switch (input)
+          {
+            case "A1":
+              // code to purchase the first element
+              index = 0;
+              break;
+            case "A2":
+              // code to purchase the second element, etc
+              index = 1;
+              break;
+            case "A3":
+              // code to purchase the first element
+              index = 2;
+              break;
+            case "A4":
+              // code to purchase the second element, etc
+              index = 3;
+              break;
+            case "B1":
+              // code to purchase the first element
+              index = 4;
+              break;
+            case "B2":
+              // code to purchase the second element, etc
+              index = 5;
+              break;
+            case "B3":
+              // code to purchase the first element
+              index = 6;
+              break;
+            case "B4":
+              // code to purchase the second element, etc
+              index = 7;
+              break;
+            case "C1":
+              // code to purchase the first element
+              index = 8;
+              break;
+            case "C2":
+              // code to purchase the second element, etc
+              index = 9;
+              break;
+            case "C3":
+              // code to purchase the first element
+              index = 10;
+              break;
+            case "C4":
+              // code to purchase the second element, etc
+              index = 11;
+              break;
+            case "D1":
+              // code to purchase the first element
+              index = 12;
+              break;
+            case "D2":
+              // code to purchase the second element, etc
+              index = 13;
+              break;
+            case "D3":
+              // code to purchase the first element
+              index = 14;
+              break;
+            case "D4":
+              // code to purchase the second element, etc
+              index = 15;
+              break;
+            default:
+              textBox2.Text = "not possible";
+              break;
+          }
+          textBox2.Text = machine.buyProduct(index);
+        }
+        else
+        {
+          textBox2.Text = string.Format("Not a valid item {0}", textBox1.Text.Length);
+        }
+      }
+    }
+  }
 }
--- VendingMachineGUI/VendingMachineGUI/VendingMachine.cs
+++ VendingMachineGUI/VendingMachineGUI/VendingMachine.cs
@@ -6,109 +6,110 @@ using System.Threading.Tasks;
 
 namespace VendingMachineGUI
 {
-    class VendingMachine
+  class VendingMachine
+  {
+    private List<Product> products;
+    public CoinBox coins;
+    public CoinBox currentCoins;
+
+    public VendingMachine(System.Windows.Forms.PictureBox[] boxes) // Fix this
     {
-        private List<Product> products;
-        public CoinBox coins;
-        public CoinBox currentCoins;
+      products = new List<Product>();
+      int elementStart = 0;
+      System.Windows.Forms.PictureBox[] pictureBoxes = { boxes[elementStart], boxes[elementStart + 1] };
+      Product temp = new Product("Cool Ranch Doritos", 1.25, 0, pictureBoxes);
+      products.Add(temp);
+      //for (int i = 0; i < 16; i++)
+      //{
+      //    System.Windows.Forms.PictureBox[] pictureBoxes = { boxes[elementStart], boxes[elementStart + 1] };
+      //    products.Add(new Product("Cool Ranch Doritos", 1.25, 0, pictureBoxes));
+      //}
+      //new Product("Cool Ranch Doritos", 1.25, 0, { boxes[0], boxes[1] }),
+      //new Product("Doritos", 1.25, 0, ref { boxes[2], boxes[3]}),
+      //new Product("Cheetos", 1.25, 0, ref { boxes[4], boxes[5] }),
+      //new Product("Funyuns", 1.25, 0, ref { boxes[6], boxes[7] }),
+      //new Product("Snickers", 1.50, 0, ref boxes[8], ref boxes[9]),
+      //new Product("Milky Way", 1.50, 0, ref boxes[10], ref boxes[11]),
+      //new Product("Twix", 1.50, 0, ref boxes[12], ref boxes[13]),
+      //new Product("3 Musketeers", 1.50, 0, ref boxes[14], ref boxes[15]),
+      //new Product("Water", 1.25, 0, ref boxes[16], ref boxes[17]),
+      //new Product("Gatorade", 1.25, 0, ref boxes[18], ref boxes[19]),
+      //new Product("", 1.25, 0, ref boxes[20], ref boxes[21]),
+      //new Product("", 1.25, 0, ref boxes[22], ref boxes[23]),
+      //new Product("", 1, 0, ref boxes[24], ref boxes[25]),
+      //new Product("", 1, 0, ref boxes[26], ref boxes[27]),
+      //new Product("", 1, 0, ref boxes[28], ref boxes[29]),
+      //new Product("", 1, 0, ref boxes[30], ref boxes[31])
+      coins = new CoinBox();
+      currentCoins = new CoinBox();
+    } // should we use switch cases for accessing elements vs. converting A1, etc to indexes??;
 
-        public VendingMachine(System.Windows.Forms.PictureBox[] boxes) // Fix this
-        {
-            products = new List<Product>();
-            int elementStart = 0;
-            System.Windows.Forms.PictureBox[] pictureBoxes = { boxes[elementStart], boxes[elementStart + 1] };
-            Product temp = new Product("Cool Ranch Doritos", 1.25, 0, pictureBoxes);
-            products.Add(temp);
-            //for (int i = 0; i < 16; i++)
-            //{
-            //    System.Windows.Forms.PictureBox[] pictureBoxes = { boxes[elementStart], boxes[elementStart + 1] };
-            //    products.Add(new Product("Cool Ranch Doritos", 1.25, 0, pictureBoxes));
-            //}
-            //new Product("Cool Ranch Doritos", 1.25, 0, { boxes[0], boxes[1] }),
-            //new Product("Doritos", 1.25, 0, ref { boxes[2], boxes[3]}),
-            //new Product("Cheetos", 1.25, 0, ref { boxes[4], boxes[5] }),
-            //new Product("Funyuns", 1.25, 0, ref { boxes[6], boxes[7] }),
-            //new Product("Snickers", 1.50, 0, ref boxes[8], ref boxes[9]),
-            //new Product("Milky Way", 1.50, 0, ref boxes[10], ref boxes[11]),
-            //new Product("Twix", 1.50, 0, ref boxes[12], ref boxes[13]),
-            //new Product("3 Musketeers", 1.50, 0, ref boxes[14], ref boxes[15]),
-            //new Product("Water", 1.25, 0, ref boxes[16], ref boxes[17]),
-            //new Product("Gatorade", 1.25, 0, ref boxes[18], ref boxes[19]),
-            //new Product("", 1.25, 0, ref boxes[20], ref boxes[21]),
-            //new Product("", 1.25, 0, ref boxes[22], ref boxes[23]),
-            //new Product("", 1, 0, ref boxes[24], ref boxes[25]),
-            //new Product("", 1, 0, ref boxes[26], ref boxes[27]),
-            //new Product("", 1, 0, ref boxes[28], ref boxes[29]),
-            //new Product("", 1, 0, ref boxes[30], ref boxes[31])
-            coins = new CoinBox();
-            currentCoins = new CoinBox();
-        } // should we use switch cases for accessing elements vs. converting A1, etc to indexes??;
-        
 
-        public void restockProducts()
-        {
-            foreach (Product product in products)
-            {
-                product.Quantity = 3;
-                product.Falling.Visible = true;
-                product.Display.Visible = true;
-            }
-        }
-
-        public List<Product> getProductTypes()
-        {
-            List<Product> types = new List<Product>();
-            for (int i = 0; i < products.Count; i++)
-            {
-                Product entry = products[i];
-                types.Add(entry);
-            }
-            return types;
-        }
-
-        public void addProduct(Product p)
-        {
-            products.Add(p);
-        }
-
-        public double addCoin(Coin c)
-        {
-            currentCoins.addCoin(c);
-            return currentCoins.getValue();
-        }
-
-        public string buyProduct(int element)
-        {
-            if (products[element].Quantity == 0) return string.Format("Out of Product {0}", products[element].getDescription());
-            else
-            {
-                double balance = currentCoins.getValue();
-                double price = products[element].getPrice();
-                if (price <= balance)
-                {
-                    double payment = balance - price;
-                    products[element].Quantity--;
-                    coins.addCoins(currentCoins);
-                    currentCoins.removeAllCoins();
-                    return string.Format("Purchased {0}", products[element].getDescription());
-                }
-                else
-                {
-                    return string.Format("Insufficient money, {0} inserted", balance.ToString("C"));
-                }
-            }
-        }
-
-        public bool productsAvailable()
-        {
-            return products.Count > 0;
-        }
-
-        public double removeMoney()
-        {
-            double r = coins.getValue();
-            coins.removeAllCoins();
-            return r;
-        }
+    public void restockProducts()
+    {
+      foreach (Product product in products)
+      {
+        product.Quantity = 3;
+        product.Falling.Visible = true;
+        product.Display.Visible = true;
+      }
     }
+
+    public List<Product> getProductTypes()
+    {
+      List<Product> types = new List<Product>();
+      for (int i = 0; i < products.Count; i++)
+      {
+        Product entry = products[i];
+        types.Add(entry);
+      }
+      return types;
+    }
+
+    public void addProduct(Product p)
+    {
+      products.Add(p);
+    }
+
+    public double addCoin(Coin c)
+    {
+      currentCoins.addCoin(c);
+      return currentCoins.getValue();
+    }
+
+    public string buyProduct(int element)
+    {
+      if (element == 99) return ("Not a valid item");
+      if (products[element].Quantity == 0) return string.Format("Out of Product {0}", products[element].getDescription());
+      else
+      {
+        double balance = currentCoins.getValue();
+        double price = products[element].getPrice();
+        if (price <= balance)
+        {
+          double payment = balance - price;
+          products[element].Quantity--;
+          coins.addCoins(currentCoins);
+          currentCoins.removeAllCoins();
+          return string.Format("Purchased {0}", products[element].getDescription());
+        }
+        else
+        {
+          return string.Format("Insufficient money, {0} inserted", balance.ToString("C"));
+        }
+      }
+    }
+
+    public bool productsAvailable()
+    {
+      return products.Count > 0;
+    }
+
+    public double removeMoney()
+    {
+      double r = coins.getValue();
+      coins.removeAllCoins();
+      return r;
+    }
+  }
 }
